556ab46b867bc992fc2b96763272258a584384b9
Brandon Antonelli <bantonelli07@gmail.com>
2018-10-05 18:14:30 -0400

    Start Scaffolding prodticket module functions - TODO: - Write test suite for this module - Come up with algorithms for these functions - Implement the function code

---
+++ utils/prodticket.js
@@ -0,0 +1,20 @@
+const _ = require("lodash");
+const stringOps = require("./string-ops");
+
+function getProgramTitle (ticketHTML) {
+    return "";
+}
+
+function getProgramByline (ticketHTML) {
+    return "";
+}
+
+function getProgramMainContribs (ticketHTML) {
+    return "";
+}
+
+
+
+module.exports = {
+
+}
