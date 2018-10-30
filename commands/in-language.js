17600199e3d7fda3adcf6301b56df32589e0b6a2
Brandon Antonelli <bantonelli07@gmail.com>
2018-10-30 17:23:24 -0400

    Complete il command (commands.inLanguage) - Tested multiple combinations of addons - Tested articleID / articleTitle - Tested pasting into D2 - TODO: 	- Start developing article-based commands

--- commands/in-language.js
+++ commands/in-language.js
@@ -21,83 +21,223 @@ const inLanguageHelp = `
 Generates code for in language add-ons. ${N}Use flags for components you wish to include.`;
 
 const languageChoices = _.keys(languages);
+const outputFile = 'inLanguage.xml'; // Make dynamic considering ArticleID_inLanguage.xml 
+let infoObject = {
+    articleID: null,
+    articleTitle: "",
+    addons: {
+        expertCommentary: {
+            has: false,
+            languages: []
+        },
+        downloadablePDF: {
+            has: false, 
+            languages: []
+        },
+        transcriptPDF: {
+            has: false,
+            languages: []
+        },
+        subtitles: {
+            has: false, 
+            languages: []
+        }
+    }
+};
 
-let outputFile = 'inLanguage.xml';
+// PROMPTS 
+// ------------------------------------------------------------
+let articleTitlePrompt = function (self) {
+    return self.prompt({
+        type: 'input',
+        name: 'title',
+        message: 'Please Enter the Article Title: ',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return ('You must enter a title!');
+            } else {
+                return true;
+            }
+        }                    
+    });
+};
+
+let expertCommentaryPrompt = function (self) {
+    return self.prompt({
+        type: 'confirm',
+        name: 'expertCommentary',
+        message: 'Has Expert Commentary?'                  
+    });
+};
+
+let downloadablePDFPrompt = function (self) {
+    return self.prompt({
+        type: 'confirm',
+        name: 'downloadablePDF',
+        message: 'Has Downloadable PDF?'                  
+    });
+};
+
+let transcriptPDFPrompt = function (self) {
+    return self.prompt({
+        type: 'confirm',
+        name: 'transcriptPDF',
+        message: 'Has Transcript PDF?'                  
+    });
+};
+
+let subtitlesPrompt = function (self) {
+    return self.prompt({
+        type: 'confirm',
+        name: 'subtitles',
+        message: 'Has Subtitles?'                  
+    });
+};
+
+let languagePrompt = function (self) {
+    return self.prompt({
+        type: 'checkbox',
+        name: 'languages',
+        choices: languageChoices,
+        message: `Please choose which languages.`,
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return ('You must choose at least 1 language');
+            } else {
+                return true;
+            }
+        }                    
+    });
+};
+
+// PROMISE THEN CALLBACK
+let promiseCallback = async function (self, callback, answers, addOn, nextFunction) {
+    if (answers) {
+        // message = 'Please choose which addons you need!'
+        infoObject.addons[addOn].has = answers[addOn];
+        if (answers[addOn]) {
+            // Get Languages 
+            try {
+                await languagePrompt(self)
+                .then((answers) => {
+                    if (answers) {
+                        infoObject.addons[addOn].languages = answers.languages;
+                    } else {
+                        self.log(`Not getting answers for ${addOn} languages`);
+                        callback();
+                    }
+                });
+            } catch (err) {
+                self.log(err);
+                callback();
+            }
+        } 
+        if (nextFunction) {
+            return nextFunction(self);                                         
+        } else {
+            callback();
+        }                
+    } else {
+        self.log(`Not getting answers for ${addOn}`);
+        callback();
+    } 
+} 
+
+
+// BUILD FUNCTION LOGIC 
+// ------------------------------------------------------------
+
+let buildFinalOutput = function (self) {   
+    var tocElements = [];
+    var keys = _.keys(infoObject.addons);
+    var currentKey = "";
+    var addonObject = null;
+    var languageString = null;
+    var languageObject = null;
+    for (var i = 0; i < 4; i++) {
+        currentKey = keys[i];
+        addonObject = infoObject.addons[currentKey];
+        // self.log("CURRENT KEY: ", keys[i]);
+        // self.log("KEYS: ", keys);
+        // self.log("ADDON OBJECT: ", addonObject);        
+        if (addonObject.has) {
+            for (var z = 0; z < addonObject.languages.length; z++) {
+                // (articleID, language, programTitle)
+                languageString = addonObject.languages[z];
+                languageObject = languages[languageString];
+                tocElements.push(snippets.inLanguage[currentKey](infoObject.articleID, languageObject, infoObject.articleTitle));
+            }
+        } 
+    }
+    // self.log(tocElements);
+    return tocElements;
+}
+
+
 
 // EXPORT
 // ------------------------------------------------------------
 module.exports = function (vorpal) {
     let chalk = vorpal.chalk;    
     vorpal
-    .command('il <articleID> <articleTitle>', inLanguageHelp)
-    .option('--ec')
-    .option('--pdf')
-    .option('--tpdf')
-    .option('--sub')
+    .command('il <articleID>', inLanguageHelp)
+    // .parse(function (command, args) { 
+    //     args.articleID = String(args.articleID);
+    //     return command + ` ` + args.articleID;   
+    // })
+    .types({string: ['_']})
     .action(function(args, callback) {
-        let articleID = args.articleID;
-        let articleTitle = args.articleTitle;
-        // this.log(args); WORKS 
-        let inLanguageFunction; 
-        let message = `Please choose which languages.`;
+        // this.log("RAW ARTICLE ID: ", args.articleID);
+        infoObject.articleID = args.articleID;        
         let self = this;
-        // self.log("Current Working Dir: ", cliTools.getOutputDirectory()); WORKS
-        if (args.options.ec) {
-            inLanguageFunction = snippets.inLanguage.expertCommentary;
-        } else if (args.options.pdf) {
-            inLanguageFunction = snippets.inLanguage.downloadablePDF;
-        } else if (args.options.tpdf) {
-            inLanguageFunction = snippets.inLanguage.transcriptPDF;
-        } else {
-            inLanguageFunction = snippets.inLanguage.subtitles;
-        }
-        var chosenLanguages = self.prompt({
-            type: 'checkbox',
-            name: 'languageChooser',
-            choices: languageChoices,
-            message: message,
-            validate: function (answer) {
-                if (answer.length < 1) {
-                    return ('You must choose at least 1 language');
-                } else {
-                    return true;
-                }
-            }
-        }, (answers) => {            
-            // if (answers) {
-            //     var xmlString = "";
-            //     var result = "";
-            //     var language = "";
-            //     var tocElements = [];
-            //     // self.log("FUNCTION: ", inLanguageFunction); WORKS 
-            //     for (var i = 0; i < answers.length; i++) {
-            //         language = languages[answers[i]];
-            //         tocElements.push(inLanguageFunction(articleID, language, articleTitle));
-            //     }
-            //     self.log("LANGUAGE OBJECT: ", language);
-            //     self.log("ANSWERS: ", answers);
-            //     self.log(tocElements);
-            //     for (var i = 0; i < tocElements.length; i++) {
-            //         xmlString = utils.xmlOps.objectToXMLString(tocElements[i].toObjectLiteral());
-            //         result += `${xmlString}\n\n`;
-            //     } 
-            //     callback();   
-            //     // il 000000 test --ec    
-            //     // try {                                                             
-            //     //     utils.cliTools.writeOutputFile(outputFile, result);
-            //     //     callback();                                     
-            //     // } catch (error) {
-            //     //     self.log(error);
-            //     //     callback(); 
-            //     // }                                
-            // } 
-            // else {
-            //     self.log('Not getting answers');
-            //     callback();
-            // }      
-            return answers;                      
+
+        // Get TITLE
+        articleTitlePrompt(self)
+        .then(answers => {           
+            // Get Has Expert Commentary?
+            if (answers) {
+                infoObject.articleTitle = answers.title;
+                return expertCommentaryPrompt(self);
+            } else {
+                self.log('Not getting answer for article title');
+                callback();
+            }  
+        })
+        .then((answers) => {
+            // Get Has Downloadable PDF?
+            return promiseCallback(self, callback, answers, "expertCommentary", downloadablePDFPrompt);
+        })
+        .then((answers) => {
+            // Get Has Transcript PDF?
+            return promiseCallback(self, callback, answers, "downloadablePDF", transcriptPDFPrompt);
+        })
+        .then((answers) => {
+            // Get Has Subtitles?
+            return promiseCallback(self, callback, answers, "transcriptPDF", subtitlesPrompt);
+        })
+        .then((answers) => {
+            // Build Final Output
+            return promiseCallback(self, callback, answers, "subtitles", buildFinalOutput);
+        })
+        .then((tocElements) => {
+            var result = "";
+            var xmlString = "";
+            for (var i = 0; i < tocElements.length; i++) {
+                xmlString = utils.xmlOps.objectToXMLString(tocElements[i].toObjectLiteral());
+                result += `${xmlString}\n\n`;
+            }    
+            try {
+                result = utils.cleanHTML.cleanEntities(result);
+                utils.cliTools.writeOutputFile(outputFile, result);
+                callback();                                     
+            } catch (error) {
+                self.log(error);
+                callback(); 
+            }   
+        }) 
+        .catch((err) => {
+            self.log(err);
         });
-        self.log("LANGUAGES: ", chosenLanguages);
     });
 };
 
