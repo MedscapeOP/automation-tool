bef10d3ea3a660d099ec52cd3545255beee142ff
Brandon Antonelli <bantonelli07@gmail.com>
2019-03-19 16:08:05 -0400

    Merge table_cleanup with testing branch - Update test files

--- tests/articles/input/test-and-teach/tables.js
+++ tests/articles/input/test-and-teach/tables.js
@@ -4,155 +4,157 @@ module.exports = [
         textBlock: `
         <p><strong>Table 1. History and Physical Examination Findings</strong></p>
 
-        <table>
+
+        <table class = "inline_data_table">
+        <tbody xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:dctm="http://www.documentum.com" xmlns:fmt="http://java.sun.com/jstl/fmt" xmlns:jsp="jsp">
         <tr>
         <td><p><strong>History</strong></p>
         </td>
         <td><p><strong>Findings</strong></p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>Medical</p>
         </td>
         <td><p>&#8226;  He is sexually active with multiple partners, both men and women, and has a history of dysuria</p>
-
+        
         <p>&#8226;      Three past episodes of uveitis that were treated by an ophthalmologist</p>
-
+        
         <p>&#8226;      Past episode of left foot plantar fasciitis that was treated by a podiatrist</p>
-
+        
         <p>o    Currently has left-sided Achilles tendonitis, diagnosed by his primary care physician</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>Medications</p>
         </td>
         <td><p>Naproxen 220 mg BID</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>Allergies</p>
         </td>
         <td><p>NKDA</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>Surgical</p>
         </td>
         <td><p>None </p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>Family</p>
         </td>
         <td><p>Mother: 65, alive</p>
-
+        
         <p>Father: 68, alive</p>
-
+        
         <p>No siblings</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>Social</p>
         </td>
         <td><p>Single</p>
-
+        
         <p>Alcohol: social (3-5 beers per week)</p>
-
+        
         <p>Tobacco: none</p>
-
+        
         <p>Illicit drugs: none</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p><strong>Physical Examination</strong></p>
         </td>
         <td><p><strong>Findings</strong></p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>Vital signs</p>
         </td>
         <td><p>BP 128/84 mm Hg; pulse rate 75/min, regular; respirations 12/min; temperature 98.7&#8304;F; O<sub>2</sub> saturation 95%, BMI 28 kg/m<sup>2</sup></p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>General</p>
         </td>
         <td><p>In no acute distress </p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>Skin</p>
         </td>
         <td><p>Normal, no evidence of psoriasis</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>HEENT</p>
         </td>
         <td><p>Normal</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>Pulmonary</p>
         </td>
         <td><p>Normal breath sounds bilaterally, no adventitious sounds</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>Heart</p>
         </td>
         <td><p>Heart sounds normal, no murmur</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>Abdomen</p>
         </td>
         <td><p>Nontender, no organomegaly</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>Musculoskeletal</p>
         </td>
         <td><p>&#8226;  Peripheral joints: no synovitis or tenderness</p>
-
+        
         <p>&#8226;      Tenderness over bilateral sacroiliac joints</p>
-
+        
         <p>&#8226;      Schober's test: 4 cm</p>
-
+        
         <p>&#8226;      Chest expansion: 1.5 cm </p>
-
+        
         <p>&#8226;      Tenderness on L plantar fascia insertion on L calcaneum on palpation </p>
-
+        
         <p>&#8226;      Tenderness and swelling at the insertion of the L Achilles tendon</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>Neurological</p>
         </td>
         <td><p>No focal deficit</p>
         </td>
         </tr>
-
-        </table>
-        <p>&lt;&lt;CAPTION&gt;&gt;BMI = body mass index; BP = blood pressure; HEENT = head, eyes, ears, neck, throat; NKDA = no known drug allergies.</p>
-
+        
+        </tbody></table>
+        <blockquote>&lt;&lt;CAPTION&gt;&gt;BMI = body mass index; BP = blood pressure; HEENT = head, eyes, ears, neck, throat; NKDA = no known drug allergies.</blockquote>
+        
         <p><strong>&lt;&lt;end table 1&gt;&gt;</strong></p>
         `,
         label: `Table 1. History and Physical Examination Findings`,
@@ -163,122 +165,126 @@ module.exports = [
         type: "table",
         textBlock: `
         <p><strong>Table 2. Laboratory Tests</strong></p>
-        <table>
+
+
+        <table class = "inline_data_table">
+        <tbody xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:dctm="http://www.documentum.com" xmlns:fmt="http://java.sun.com/jstl/fmt" xmlns:jsp="jsp">
         <tr>
         <td><p><strong>CBC</strong></p>
         </td>
         <td><p><strong>Results</strong></p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>WBC</p>
         </td>
         <td><p> 7.8 K/cu mm</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>HGB</p>
         </td>
         <td><p> 14.0 g/dL</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>HCT</p>
         </td>
         <td><p> 43%</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>Platelet count</p>
         </td>
         <td><p> 306 K/cu mm</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p><strong>Metabolic Panel</strong></p>
         </td>
         <td><p><strong>Results</strong></p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>Sodium</p>
         </td>
         <td><p>137 mEq/L</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>Potassium</p>
         </td>
         <td><p>5.1 mEq/L</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>Chloride</p>
         </td>
         <td><p>101 mEq/L</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>CO<sub>2</sub></p>
         </td>
         <td><p>25 mEq/L</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>Creatinine</p>
         </td>
         <td><p>0.9 mg/dL</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>BUN</p>
         </td>
         <td><p>17 mg/dL</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>eGFR</p>
         </td>
         <td><p>100 mL/min/1.73 m<sup>2</sup></p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>Glucose, HbA<sub>1C</sub></p>
         </td>
         <td><p>98 mg/dL</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>ESR</p>
         </td>
         <td><p>21 mm/h</p>
         </td>
         </tr>
-
+        
         <tr>
         <td><p>CRP</p>
         </td>
         <td><p>0.6 mg/dl </p>
         </td>
         </tr>
-
-        </table>
-        <p>&lt;&lt;CAPTION&gt;&gt;BUN = blood urea nitrogen; CBC = complete blood cell count; CRP = C-reactive protein; eGFR = estimated glomerular filtration rate; ESR = erythrocyte sedimentation rate; HbA1c = glycated hemoglobin; HCT = hematocrit; HGB = hemoglobin; WBC = white blood cell.</p>
-
+        
+        </tbody></table>
+        <blockquote>&lt;&lt;CAPTION&gt;&gt;BUN = blood urea nitrogen; CBC = complete blood cell count; CRP = C-reactive protein; eGFR = estimated glomerular filtration rate;
+        ESR = erythrocyte sedimentation rate; HbA1c = glycated hemoglobin; HCT = hematocrit; HGB = hemoglobin; WBC = white blood cell.</blockquote>
+        
         <p><strong>&lt;&lt;end table 2&gt;&gt;</strong></p>
         `,
         label: `Table 2. Laboratory Tests`,
