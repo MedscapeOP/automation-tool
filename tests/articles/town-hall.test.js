e08ba32bf189b6bc7d30c26354919088b9e14c09
Brandon Antonelli <bantonelli07@gmail.com>
2019-09-05 16:19:02 -0400

    Update townhall Enduring test files

--- tests/articles/town-hall.test.js
+++ tests/articles/town-hall.test.js
@@ -38,13 +38,10 @@ describe('Town Hall', function () {
 
     beforeEach(function() {
         prodTicket = fs.readFileSync(__dirname + '/input/town-hall/article-enduring.html', 'utf8');
-        // completeClinicalContext = utils.xmlOps.objectToXMLString(require('./input/clinical-context'));
 
         completeTownHallEnduring = fs.readFileSync(__dirname + '/input/town-hall/town-hall-enduring.xml').toString();
 
         completeTownHallReg = fs.readFileSync(__dirname + '/input/town-hall/town-hall-reg.jsp').toString();
-        
-        completeSlidesTOCs = fs.readFileSync(__dirname + '/input/first-response/slides-fr.xml').toString();
 
         program = app.config.programs.townHall;
 
@@ -70,32 +67,6 @@ describe('Town Hall', function () {
 
             expect(result).to.equalIgnoreSpaces(completeTownHallEnduring);
         });
-
-        // it('should return complete XML string of First Response article - with LLA', function () {
-        //     program.hasCollectionPage = false;
-        //     program.hasPeerReviewer = true;
-        //     // Tested and working with For your patient
-        //     program.hasForYourPatient = false;
-        //     program.hasLLA = true;
-        //     // program.articleID = "897160";
-
-        //     var result = firstResponse.buildFirstResponse(prodTicket, program).toObjectLiteral();
-
-        //     // console.log("RESULT: ", JSON.stringify(result, undefined, 2));
-
-        //     var differences = [                
-        //     ];
-
-        //     result = utils.cleanHTML.cleanEntities(utils.xmlOps.objectToXMLString(result));
-        //     // result = utils.xmlOps.objectToXMLString(result);
-
-        //     fs.writeFileSync(__dirname + '/output/first-response/finished-fr.xml', result);
-
-        //     // console.log("RESULT: ", result);
-        //     // utils.xmlOps.writeXMLFromObject(result, __dirname + "/output/spotlight/finished-sl.xml");
-
-        //     expect(result).to.equalIgnoreSpaces(completeTownHallReg);
-        // });
     });
 });
 
