1d54c51b97020ff2e9410d3f9c41a12adbe4f7c1
Brandon Antonelli <bantonelli07@gmail.com>
2019-10-08 12:03:02 -0400

    Fix abbreviations bug in TH Cert tickets - Use last instance of Abbreviations match as the go to for textblock - Implmented with getAllMatchesInOrder()

--- prodticket/find-abbreviations.js
+++ prodticket/find-abbreviations.js
@@ -42,16 +42,47 @@ exportObject[config.programs.townHall.codeName] = function (ticketHTML) {
     return exportObject[config.programs.spotlight.codeName](ticketHTML);
 }
 
+
+var abbreviationsRegexArray = [
+    /<p>Abbreviations<\/p>/gi,    
+    /<strong>Abbreviations/g
+];
+
+var endAbbreviationsRegexArray = [
+    /<a name=".*"><\/a>Transcript/g,
+    /<\/html>/g,
+    /<a name=".*"><\/a>Disclaimer/g
+];
+
 // TownHall Cert Page 
 exportObject[config.programs.townHallCert.codeName] = function (ticketHTML) {
-    var {textBlock: rawAbbreviations, label: abbrLabel} = stringOps.getTextBlock(ticketHTML, /Abbreviations/g, /<a name=".*"><\/a>Transcript/g);
-    console.log("ABBREVIATIONS: ", (rawAbbreviations));
-    // return "";
-    if (stringOps.isEmptyString(rawAbbreviations) || stringOps.isBlankOrWhiteSpace(rawAbbreviations) || rawAbbreviations.length < 5) {
-        throw new Error("No abbreviations found in the prodticket");
+
+    // var startRegExp = ticketHTML.match(/Abbreviations(?!Abbreviations)/i);
+    // if (startRegExp.index) {
+    //     startRegExp = stringOps.getNextRegex(ticketHTML.substring(startRegExp.index), abbreviationsRegexArray);
+    // } else {
+    //     throw new Error("No abbreviations found in the prodticket");
+    // }
+    var startRegExp = stringOps.getAllMatchesInOrder(ticketHTML, abbreviationsRegexArray);
+    startRegExp = startRegExp[startRegExp.length - 1];
+
+    var endRegExp = stringOps.getAllMatchesInOrder(ticketHTML, endAbbreviationsRegexArray);
+    endRegExp = endRegExp[endRegExp.length - 1];
+    
+    if (endRegExp != -1 && startRegExp != -1) {
+        startRegExp = startRegExp.symbol;
+        endRegExp = endRegExp.symbol;
+        // console.log("START REGEX: ", startRegExp);
+        var {textBlock: rawAbbreviations, label: abbrLabel} = stringOps.getTextBlock(ticketHTML, startRegExp, endRegExp, true, false);
+        console.log("ABBREVIATIONS: ", (rawAbbreviations));
+        if (stringOps.isEmptyString(rawAbbreviations) || stringOps.isBlankOrWhiteSpace(rawAbbreviations) || rawAbbreviations.length < 5) {
+            throw new Error("No abbreviations found in the prodticket");
+        } else {
+            rawAbbreviations = cleanHTML.singleLine(cleanHTML.abbreviations(rawAbbreviations)).trim();
+            return '<p>' + stringOps.findLastAndReplace(rawAbbreviations, '<br/>', "") + '</p>';        
+        } 
     } else {
-        rawAbbreviations = cleanHTML.singleLine(cleanHTML.abbreviations(rawAbbreviations)).trim();
-        return '<p>' + stringOps.findLastAndReplace(rawAbbreviations, '<br/>', "") + '</p>';        
+        throw new Error("No abbreviations found in the prodticket");
     }
 }
 
