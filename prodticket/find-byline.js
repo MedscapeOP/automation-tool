a0a87c4bf5ae31ad9a3f7aafe9aa6885cb74441d
Brandon Antonelli <bantonelli07@gmail.com>
2020-01-30 15:59:24 -0500

    Fix byline and teaser prodticket modules - Both find functions will now work for both old and new byline placement

--- prodticket/find-byline.js
+++ prodticket/find-byline.js
@@ -25,33 +25,40 @@ exportObject[config.programs.clinicalBrief.codeName] = function (ticketHTML) {
 
 
 // Spotlight
-var spotlightStartRegExps = [
+let spotlightStartRegExps = [
     /<strong>Faculty\/Author\(s\) Byline\(s\): &#953;.*/g,
     /<strong>Faculty\/Author\(s\) Byline\(s\).*/g,
     /<strong>Faculty Byline/g,
 ];
+
+let spotlightEndRegExps = [
+    /.*<strong>Target Audience.*/g,
+    /.*Faculty affiliations are confirmed to be accurate.*/g
+];
+
 var spotlightRemoveRegExps = [
     /.*Indicate thumbnail URL (?:&#953;){0,}.*/g
 ];
 
 exportObject[config.programs.spotlight.codeName] = function (ticketHTML) {
-    var startRegExp = stringOps.getUsableRegExp(ticketHTML, spotlightStartRegExps);
-    var endRegExp = /<strong>Target Audience/g;
-    if (startRegExp != -1) {
-        var {textBlock: byline} = stringOps.getTextBlock(ticketHTML, startRegExp, endRegExp, true, false);
-
-        // console.log("BYLINE: ", byline);
-        for (var i = 0; i < spotlightRemoveRegExps.length; i++) {
-            byline = byline.replace(spotlightRemoveRegExps[i], "");
-        }
+    var startRegExp = stringOps.getAllMatchesInOrder(ticketHTML, spotlightStartRegExps);
+    var endRegExp = stringOps.getAllMatchesInOrder(ticketHTML, spotlightEndRegExps);
+    console.log("Start RegExp", startRegExp)
+    console.log("EndRegex: ", endRegExp)
+    if (startRegExp.length <= 0 || endRegExp.length <=0) {
+        throw new Error("No byline found in the prodticket");
+    } else {
+        let byline = stringOps.getTextBlock(ticketHTML, startRegExp[startRegExp.length - 1].symbol, endRegExp[endRegExp.length - 1].symbol, true, false).textBlock;
 
         if (stringOps.isBlankOrWhiteSpace(byline) || stringOps.isEmptyString(byline)) {
             throw new Error("No byline found in the prodticket");
         } else {
+            console.log("BYLINE: ", byline);
+            for (var i = 0; i < spotlightRemoveRegExps.length; i++) {
+                byline = byline.replace(spotlightRemoveRegExps[i], "");
+            }
             return cleanHTML.singleLine(cleanHTML.plainText(byline)).trim();
         }
-    } else {
-        throw new Error("No byline found in the prodticket");
     }
 }
 
